ed0853d57ec8cbc4ebc0457ef16e2b3ced772d4c
alyrchikov@roox.ru <alyrchikov@roox.ru>
2021-05-08 22:52:18 +0300

    homework: #8 / cache

--- src/main/java/ru/algoritms/datastructures/cache/NativeCacheTest.java
+++ src/main/java/ru/algoritms/datastructures/cache/NativeCacheTest.java
@@ -6,83 +6,34 @@ import org.junit.Test;
 
 public class NativeCacheTest {
 
-    private NativeCache<String> nativeCache;
+    private NativeCache<String> cache;
 
     @Before
     public void setup() {
-        nativeCache = new NativeCache<>(17, String.class);
+        cache = new NativeCache<>(17, String.class);
     }
 
     @Test
     public void testPut() {
-        nativeCache.put("test", "test");
-        Assert.assertEquals("test", nativeCache.slots[13]);
-        Assert.assertEquals("test", nativeCache.values[13]);
-        Assert.assertEquals(1, nativeCache.hits[13]);
+        cache.put("test", "test");
+        Assert.assertEquals("test", cache.slots[13]);
+        Assert.assertEquals("test", cache.values[13]);
+        Assert.assertEquals(1, cache.hits[13]);
 
-        nativeCache.put("test", "new value");
-        Assert.assertEquals("test", nativeCache.slots[13]);
-        Assert.assertEquals("new value", nativeCache.values[13]);
-        Assert.assertEquals(2, nativeCache.hits[13]);
+        cache.put("test", "new test");
+        Assert.assertEquals("test", cache.slots[13]);
+        Assert.assertEquals("new test", cache.values[13]);
+        Assert.assertEquals(2, cache.hits[13]);
 
-        nativeCache.put("notest", "notest");
-        Assert.assertEquals("notest", nativeCache.slots[15]);
-        Assert.assertEquals("notest", nativeCache.values[15]);
-        Assert.assertEquals(1, nativeCache.hits[15]);
+        cache.put("mother", "motherlorde");
+        Assert.assertEquals("mother", cache.slots[9]);
+        Assert.assertEquals("motherlorde", cache.values[9]);
+        Assert.assertEquals(1, cache.hits[9]);
 
-        nativeCache.put("notest", "new notest");
-        Assert.assertEquals("notest", nativeCache.slots[15]);
-        Assert.assertEquals("new notest", nativeCache.values[15]);
-        Assert.assertEquals(2, nativeCache.hits[15]);
+        cache.put("mother", "new motherlorde");
+        Assert.assertEquals("mother", cache.slots[9]);
+        Assert.assertEquals("new motherlorde", cache.values[9]);
+        Assert.assertEquals(2, cache.hits[9]);
 
-        nativeCache.put("hello", "hello");
-        Assert.assertEquals("hello", nativeCache.slots[0]);
-        Assert.assertEquals("hello", nativeCache.values[0]);
-        Assert.assertEquals(1, nativeCache.hits[0]);
-
-        nativeCache.put("hello", "new hello");
-        Assert.assertEquals("hello", nativeCache.slots[0]);
-        Assert.assertEquals("new hello", nativeCache.values[0]);
-        Assert.assertEquals(2, nativeCache.hits[0]);
-
-        nativeCache.put("0123456789", "0123456789");
-        Assert.assertEquals("0123456789", nativeCache.slots[16]);
-        Assert.assertEquals("0123456789", nativeCache.values[16]);
-        Assert.assertEquals(1, nativeCache.hits[16]);
-
-        nativeCache.put("0123456789", "new 0123456789");
-        Assert.assertEquals("0123456789", nativeCache.slots[16]);
-        Assert.assertEquals("new 0123456789", nativeCache.values[16]);
-        Assert.assertEquals(2, nativeCache.hits[16]);
-
-        for (int i = 1; i < 13; i++) {
-            nativeCache.slots[i] = "dummy";
-            nativeCache.hits[i] = 2;
-        }
-        Assert.assertNull(nativeCache.slots[14]);
-        Assert.assertEquals(0, nativeCache.hits[14]);
-
-        nativeCache.put("world", "world");
-        Assert.assertEquals("world", nativeCache.slots[14]);
-        Assert.assertEquals("world", nativeCache.values[14]);
-        Assert.assertEquals(1, nativeCache.hits[14]);
-
-        nativeCache.put("foo", "foo");
-        Assert.assertEquals("foo", nativeCache.slots[14]);
-        Assert.assertEquals("foo", nativeCache.values[14]);
-        Assert.assertEquals(1, nativeCache.hits[14]);
-
-        nativeCache.get("foo");
-        Assert.assertEquals(2, nativeCache.hits[14]);
-
-        nativeCache.put("foo", "new foo");
-        Assert.assertEquals("foo", nativeCache.slots[14]);
-        Assert.assertEquals("new foo", nativeCache.values[14]);
-        Assert.assertEquals(3, nativeCache.hits[14]);
-
-        nativeCache.put("bar", "bar");
-        Assert.assertEquals("bar", nativeCache.slots[8]);
-        Assert.assertEquals("bar", nativeCache.values[8]);
-        Assert.assertEquals(1, nativeCache.hits[8]);
     }
 }
